b3e8464bba14f24dccf75ab042d860bfdc3a0073
Alek <antic.alek1@gmail.com>
2018-09-23 21:53:00 +0000

    Refactoring Eternal with better parameters

--- src/engines/seq/eternal/eternal.hpp
+++ src/engines/seq/eternal/eternal.hpp
@@ -17,36 +17,39 @@ namespace otto::engines {
 
   struct Eternal : SequencerEngine {
     static constexpr int max_length = 64;
+    static constexpr int midi_max = 127;
+
     struct Channel : Properties<> {
-      Property<int> length = {this, "Length", 0, has_limits::init(0, max_length)};
-      
+      std::vector<char> sequence;
+
+      Property<int> length = {this, "Length", 0, has_limits::init(0, max_length), 
+                                    steppable::init(1)};
+      Property<int> start = {this, "Start", 0, has_limits::init(0, max_length),
+                                    steppable::init(1)};
+      Property<int, wrap> sequence_time = {this, "Sequence Time", 2, has_limits::init(0,4), steppable::init(1)};
+
+      struct Props : Properties<> {
+        Property<int, wrap> current_beat = {this, "Current Beat", 0, has_limits::init(0, max_length)};
+        Property<char, wrap> current_note = {this, "Current Note", -1, has_limits::init(-1, midi_max), steppable::init(1)};
+        Property<int, wrap> note_time = {this, "Note Time", 2, has_limits::init(0, 4), steppable::init(1)};
+      } props;
+
       void delete_sequence() {
         sequence.clear();
       }
 
       void add_note(char note) {
         sequence.push_back(note);
-        props.current_beat++;
+        props.current_beat;
       }
 
-      std::vector<char> sequence;
     } channel;
 
-    struct Props : Properties<> {
-      Property<int> current_beat = {this, "Current Beat", 0, has_limits::init(0, max_length)};
 
-      char get_current_note() {
-        return channel.sequence[current_beat];
-      }
-    } props;
 
     Eternal();
 
     audio::ProcessData<0> process(audio::ProcessData<0>) override;
-
-    void delta(int clicks) {
-      props.current_beat.set(current_beat + clicks);
-    }
 
     bool running = false;
   
